161cb95cfd988c41b36b75984733231061a8c9a9
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-08-02 18:26:07 +0700

    AnimationTranslate

--- app/src/main/java/com/example/duongtainhan555/animationtranslate/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/animationtranslate/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.duongtainhan555.animationtranslate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imgTranslate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Init()
+        Init();
+        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.custom_image_translate);
+        imgTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animation);
+            }
+        });
+    }
+
+    private void Init()
+    {
+        imgTranslate = findViewById(R.id.imgTranslate);
     }
 }
